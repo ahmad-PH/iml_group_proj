4f651adab0a7044b5c822b891d4dcc598b1f86bf
Teerapat12 <teerapat.time12@gmail.com>
2021-12-11 06:53:27 -0500

    "Cleaning code and make pg files store csv to look at later."

--- playground/bert_pg.py
+++ playground/bert_pg.py
@@ -1,11 +1,13 @@
-import torch
+import time
 import pandas as pd
 from sklearn import preprocessing
 from iml_group_proj.features.bert import get_BERT_features
 
 from iml_group_proj.evaluation import evaluate_many
 from iml_group_proj.features.common.data import load
+from iml_group_proj.models.mlp import BASIC_MLP
 from iml_group_proj.train_models import train_models
+
 
 classes, train, test = load()
 train = train.sample(frac=0.25)
@@ -16,23 +18,21 @@ y_train = le.fit_transform(train["class"])
 y_test = le.transform(test["class"])
 
 # Training Phase
-from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
 models = [
+        (BASIC_MLP, 'MLP_100'),
         # (MLPClassifier(random_state=1, max_iter=10, hidden_layer_sizes=(100, 100)), 'MLP_10'),
-        (MLPClassifier(random_state=1, max_iter=100, hidden_layer_sizes=(100, 100)), 'MLP_100'),
         # (MLPClassifier(random_state=1, max_iter=150, hidden_layer_sizes=(100, 100)), 'MLP_150'),
         ]
 
 # Hacky way to remove the sypnosis
 trained_models_1 = train_models(models, X_train[:, :X_train.shape[1]//2], y_train, {"sypnosis": False})
-trained_models_2 = train_models(models, X_train, y_train, {"sypnosis": True})
-trained_models = trained_models_1 + trained_models_2
+result_df_1 = evaluate_many(trained_models_1, X_train[:, :X_train.shape[1]//2], y_train, X_test[:, :X_test.shape[1]//2], y_test)
 
-# Evaluation
-result_df_full = evaluate_many(trained_models, X_train, y_train, X_test, y_test)
+trained_models_2 = train_models(models, X_train, y_train, {"sypnosis": True})
+result_df_2 = evaluate_many(trained_models_2, X_train, y_train, X_test, y_test)
+
+# Full Evaluation
+result_df_full = pd.concat([result_df_1, result_df_2], axis=0)
 
 print(result_df_full.head(20))
-
-result_df_full["params"] = 
-result_df_full.to_csv("_output/bert_result.csv")
+result_df_full.to_csv(f'_output/bert_result_{time.strftime("%Y%m%d-%H%M%S")}.csv')
--- playground/tfidf_pg.py
+++ playground/tfidf_pg.py
@@ -1,4 +1,4 @@
-import torch
+import time
 import pandas as pd
 from sklearn import preprocessing
 from iml_group_proj.features.simple import get_simple_features
@@ -6,15 +6,13 @@ from iml_group_proj.features.simple import get_simple_features
 from iml_group_proj.evaluation import evaluate_many
 from iml_group_proj.train_models import train_models
 from iml_group_proj.features.common.data import load
-
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
+from iml_group_proj.models.mlp import BASIC_MLP
 
 IS_BOW = False # Bag-of-words or TfIdf
 
 classes, train, test = load()
 train = train.sample(frac=0.5)
+
 X_train, X_test = get_simple_features(train, test, bow=IS_BOW, only_title=False)
 X_train = X_train.toarray()
 X_test = X_test.toarray()
@@ -26,15 +24,24 @@ y_test = le.transform(test["class"])
 # Training Phase
 models = [
         # (MLPClassifier(random_state=1, max_iter=100, verbose=True), {"hidden_layer_sizes":[(100, 100), (200, 200)]}, 'MLP_100'),
-        (MLPClassifier(random_state=1, max_iter=100, hidden_layer_sizes=(100, 100), verbose=True, early_stopping=True), None, 'MLP_100'),
+        (BASIC_MLP, None, 'MLP_100'),
         # (GaussianNB(), {},'NaiveBayes'),
         # (SVC(), {"gamma": ["auto"], "kernel": ["rbf", "sigmoid", "poly"]},'SVM'),
         # (SVC, {"gamma": "auto", "kernel": "rbf"},'SVM'),
         ]
 
-trained_models = train_models(models, X_train, y_train)
+trained_models = train_models(models, X_train, y_train, {"with_sypnosis": False})
+result_df = evaluate_many(trained_models, X_train, y_train, X_test, y_test)
+
+X_train, X_test = get_simple_features(train, test, bow=IS_BOW, only_title=True)
+X_train = X_train.toarray()
+X_test = X_test.toarray()
+trained_models_2 = train_models(models, X_train, y_train, {"with_sypnosis": True})
+result_df_2 = evaluate_many(trained_models_2, X_train, y_train, X_test, y_test)
 
 # Hacky way to remove the sypnosis
 print("Bag-of-words" if IS_BOW else "Tf-idf")
-result_df = evaluate_many(trained_models, X_train, y_train, X_test, y_test)
-print(result_df.head(20))
+result_df_full = pd.concat([result_df, result_df_2], axis=0)
+print(result_df_full.head(20))
+
+result_df_full.to_csv(f'_output/tfidf_result_{time.strftime("%Y%m%d-%H%M%S")}.csv')
